ee8de14a85aa9b2b94b81949821b89d201a73531
liudmila05 <ol.liuda.94@gmail.com>
2022-11-05 09:54:00 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+
+let changeColorId = null;
+
+startButton.addEventListener('click', changeColorStart);
+stopButton.addEventListener('click', changeColorStop);
+
+function changeColorStart() {
+    changeColorId = setInterval(() => {
+        document.body.style.background = getRandomHexColor()
+    }, 1000);
+    startButton.setAttribute("disabled", true);
+    stopButton.disabled = false;
+};
+
+function changeColorStop() {
+    clearInterval(changeColorId);
+    stopButton.setAttribute("disabled", true);
+    startButton.disabled = false;
+};
+
 
